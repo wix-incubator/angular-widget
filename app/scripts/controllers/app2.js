b84e85434708beeb4ff01383649e84d773a11e23
Shahar Talmi <shahar.talmi@gmail.com>
2014-10-29 13:47:48 +0200

    make example more colorful

--- app/scripts/controllers/app2.js
+++ app/scripts/controllers/app2.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('app2', ['ngRoute']).config(function ($routeProvider) {
+  var colors = {
+    sub1: 'pink',
+    sub2: 'purple',
+    sub3: 'yellow'
+  };
   ['sub1', 'sub2', 'sub3'].forEach(function (applicationName) {
     $routeProvider.when('/app2/' + applicationName, {
-      template: '<div>ng-view / ' + applicationName + '</div>'
+      template: '<div style="background-color: ' + colors[applicationName] + '">ng-view / ' + applicationName + '</div>'
     });
   });
 
--- app/scripts/controllers/app3.js
+++ app/scripts/controllers/app3.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('app3', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+  var colors = {
+    sub1: 'pink',
+    sub2: 'purple',
+    sub3: 'yellow'
+  };
   ['sub1', 'sub2', 'sub3'].forEach(function (applicationName) {
     $stateProvider.state(applicationName, {
       url: '/app3/' + applicationName,
-      template: '<div>ui-view / ' + applicationName + '</div>'
+      template: '<div style="background-color: ' + colors[applicationName] + '">ui-view / ' + applicationName + '</div>'
     });
   });
 
